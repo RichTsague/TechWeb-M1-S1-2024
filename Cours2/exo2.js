e6287e67d2c868d45137b4606725f85bc93e2642
RichTsague <rich-tresor.tsague-zangue@student.junia.com>
2024-10-02 13:26:22 +0200

    axo2

---
+++ Cours2/exo2.js
@@ -0,0 +1,19 @@
+// Définition d'un étudiant sans type explicite
+const Joe = {
+    nom: "Doe",
+    prenom: "John",
+    age: 20,
+    scores: [15, 18, 20, 12, 17]
+  };
+  
+  // Fonction pour calculer la moyenne
+  function calculerMoyenne(student) {
+    const total = student.scores.reduce((accumulateur, note) => accumulateur + note, 0);
+    const moyenne = total / student.scores.length;
+    return moyenne;
+  }
+  
+  // Utilisation de la fonction
+  const moyenneEtudiant = calculerMoyenne(Joe);
+  console.log(`La moyenne de ${Joe.prenom} ${Joe.nom} est ${moyenneEtudiant}`);
+  
